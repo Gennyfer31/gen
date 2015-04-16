ef88196ef470cfede8bf11581e6674d960c56205
sethyuan <sethyuan@gmail.com>
2015-04-16 22:35:48 +0800

    Update ClojureScript version and remove all intents to use REPL.

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :license "MIT"
   :url "https://github.com/sethyuan/gen"
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/clojurescript "0.0-3126"]
+                 [org.clojure/clojurescript "0.0-3196"]
                  [org.clojure/tools.cli "0.3.1"]
                  [org.clojure/core.async "0.1.346.0-17112a-alpha"]]
   :plugins [[lein-cljsbuild "1.0.5" :exclusions [org.clojure/clojure]]]
@@ -15,6 +15,7 @@
                                    :target :nodejs
                                    :source-map true
                                    :optimizations :none
+                                   :cache-analysis true
                                    :pretty-print true}}
                        {:id "release"
                         :source-paths ["src"]
